15aea9e3845edaf58edc4fdb6853901453be3ffc
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2023-11-24 20:15:08 +0200

    feat: Added OfficeAndLocation model

--- app/lib/src/models/LostAndFound.dart
+++ app/lib/src/models/LostAndFound.dart
@@ -1,9 +1,14 @@
+import 'package:GUConnect/src/models/User.dart';
+
 /// Represents a lost and found item.
 class LostAndFound {
   late String content;
   late String image;
   late String location;
   late String contact;
+  late DateTime createdAt;
+  late bool isFound;
+  late User user;
 
   /// Constructs a [LostAndFound] object with the given [content], [image], [location], and [contact].
   LostAndFound({
@@ -11,6 +16,9 @@ class LostAndFound {
     required this.image,
     required this.location,
     required this.contact,
+    required this.createdAt,
+    required this.isFound,
+    required this.user,
   });
 
   /// Constructs a [LostAndFound] object from a JSON [Map].
@@ -19,6 +27,9 @@ class LostAndFound {
     image = json['image'];
     location = json['location'];
     contact = json['contact'];
+    createdAt = DateTime.parse(json['createdAt']);
+    isFound = json['isFound'];
+    user = User.fromJson(json['user']);
   }
 
   /// Converts the [LostAndFound] object to a JSON [Map].
@@ -28,6 +39,9 @@ class LostAndFound {
     data['image'] = image;
     data['location'] = location;
     data['contact'] = contact;
+    data['createdAt'] = createdAt.toString();
+    data['isFound'] = isFound;
+    data['user'] = user.toJson();
     return data;
   }
 
@@ -38,6 +52,6 @@ class LostAndFound {
 
   @override
   String toString() {
-    return 'content: $content, image: $image, location: $location, contact: $contact';
+    return 'content: $content, image: $image, location: $location, contact: $contact, createdAt: $createdAt, isFound: $isFound, user: $user';
   }
 }
---
+++ app/lib/src/models/OfficeAndLocation.dart
@@ -0,0 +1,53 @@
+/*
+OfficeLocation	- Name
+- Latitude
+- Langitude
+- Building
+- Room
+- Type (enum professor,, TA, office, food outlet)	- Search for location
+- Get directions
+*/
+enum OfficeType { PROFESSOR, TA, OFFICE, FOOD_OUTLET }
+
+class OfficeAndLocation {
+  late String name;
+  late double latitude;
+  late double longitude;
+  late String building;
+  late String room;
+  late OfficeType type;
+
+  OfficeAndLocation({
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+    required this.building,
+    required this.room,
+    required this.type,
+  });
+
+  OfficeAndLocation.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    latitude = json['latitude'];
+    longitude = json['longitude'];
+    building = json['building'];
+    room = json['room'];
+    type = json['type'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    data['latitude'] = latitude;
+    data['longitude'] = longitude;
+    data['building'] = building;
+    data['room'] = room;
+    data['type'] = type;
+    return data;
+  }
+
+  @override
+  String toString() {
+    return 'name: $name, latitude: $latitude, longitude: $longitude, building: $building, room: $room, type: $type';
+  }
+}
