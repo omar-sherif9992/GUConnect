c352050474aae0251860844fdadaca9aad76d602
omar-sherif9992 <omar.sherif9992.com>
2023-11-25 20:07:20 +0200

    fix dep

---
+++ app/lib/src/screens/splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget  {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash>  with SingleTickerProviderStateMixin{
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
