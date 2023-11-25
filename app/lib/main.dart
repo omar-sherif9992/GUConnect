7ce51372d36848269bcde029442e6978ca6dafcc
davidsamy1 <99022820+davidsamy1@users.noreply.github.com>
2023-11-25 20:26:48 +0200

    update independencies

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:GUConnect/routes.dart';
 import 'package:GUConnect/src/screens/Home.dart';
 import 'package:GUConnect/themes/themes.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
+
 
 void main() async {
   await initializeApp();
