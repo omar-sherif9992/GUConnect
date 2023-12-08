7ca11403c456bc9f3d4a4c8b747f90d1c8d65c50
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2023-12-08 04:37:36 +0200

    feat: Added sendTagNotification, sendCommentNotification, sendLikeNotification, and sendPostApprovalNotification functions

--- app/gucconnect/index.js
+++ app/gucconnect/index.js
@@ -24,12 +24,197 @@ exports.sendNotification = functions.https.onCall((data, context) => {
         body: body,
       },
     };
-    return fcm.send(message).then((response) => {
-      return {success: true, response: "Notification sent successfully"};
-    }).catch((error) => {
-      return {success: false, response: error};
-    });
+    return fcm
+        .send(message)
+        .then((response) => {
+          return {
+            success: true,
+            response: "Notification sent successfully",
+          };
+        })
+        .catch((error) => {
+          return {success: false, response: error};
+        });
   } catch (error) {
     return {success: false, response: error};
   }
 });
+
+exports.sendTagNotification = functions.https.onCall((data, context) => {
+  /*
+    data = {
+        taggedUser: {
+            name: string,
+            token: string,
+        },
+        confessionId: string,
+        taggerName: string,
+    }
+    */
+
+  const title = `${data.taggerName} tagged you in a confession`;
+  const body = "Click to view the confession";
+  const token = data.taggedUser.token;
+  const confessionId = data.confessionId;
+
+  try {
+    const message = {
+      notification: {
+        title: title,
+        body: body,
+      },
+      token: token,
+      data: {
+        confessionId: confessionId,
+      },
+    };
+    return fcm
+        .send(message)
+        .then((response) => {
+          return {
+            success: true,
+            response: "Notification sent successfully",
+          };
+        })
+        .catch((error) => {
+          return {success: false, response: error};
+        });
+  } catch (error) {
+    return {success: false, response: error};
+  }
+});
+
+exports.sendCommentNotification = functions.https.onCall((data, context) => {
+  /*
+        data = {
+            postOwner: {
+                name: string,
+                token: string,
+            },
+            postId: string,
+            type: string [confession, event post, announcement, ...],
+            commenterName: string,
+        }
+  */
+
+  const title = `${data.commenterName} commented on your ${data.type}`;
+  const body = "Click to view the post";
+  const token = data.postOwner.token;
+  const postId = data.postId;
+
+
+  try {
+    const message = {
+      notification: {
+        title: title,
+        body: body,
+      },
+      token: token,
+      data: {
+        confessionId: postId,
+      },
+    };
+    return fcm
+        .send(message)
+        .then((response) => {
+          return {
+            success: true,
+            response: "Notification sent successfully",
+          };
+        })
+        .catch((error) => {
+          return {success: false, response: error};
+        });
+  } catch (error) {
+    return {success: false, response: error};
+  }
+});
+
+exports.sendLikeNotification = functions.https.onCall((data, context) => {
+  /*
+            data = {
+                postOwner: {
+                    name: string,
+                    token: string,
+                },
+                postId: string,
+                type: string [confession, event post, announcement, ...],
+                likerName: string,
+            }
+    */
+
+  const title = `${data.likerName} liked your ${data.type}`;
+  const body = "Click to view the post";
+  const token = data.postOwner.token;
+  const postId = data.postId;
+
+  try {
+    const message = {
+      notification: {
+        title: title,
+        body: body,
+      },
+      token: token,
+      data: {
+        confessionId: postId,
+      },
+    };
+    return fcm
+        .send(message)
+        .then((response) => {
+          return {
+            success: true,
+            response: "Notification sent successfully",
+          };
+        })
+        .catch((error) => {
+          return {success: false, response: error};
+        });
+  } catch (error) {
+    return {success: false, response: error};
+  }
+});
+
+exports.sendPostApprovalNotification = functions.https.onCall(
+    (data, context) => {
+    /*
+                data = {
+                    postOwner: {
+                        name: string,
+                        token: string,
+                    },
+                    postId: string,
+                }
+    */
+
+      const title = "Your last post was approved";
+      const body = "Click to view the post";
+      const token = data.postOwner.token;
+      const postId = data.postId;
+
+      try {
+        const message = {
+          notification: {
+            title: title,
+            body: body,
+          },
+          token: token,
+          data: {
+            confessionId: postId,
+          },
+        };
+        return fcm
+            .send(message)
+            .then((response) => {
+              return {
+                success: true,
+                response: "Notification sent successfully",
+              };
+            })
+            .catch((error) => {
+              return {success: false, response: error};
+            });
+      } catch (error) {
+        return {success: false, response: error};
+      }
+    });
