29f2f2e6ec4f68d1247d595c78b37bad4f5f42f2
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2023-12-08 02:14:39 +0200

    feat: add notification

---
+++ app/lib/src/services/notification_api.dart
@@ -0,0 +1,64 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:rxdart/rxdart.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  if (kDebugMode) {
+    print("Handling a background message: ${message.messageId}");
+    print('Message data: ${message.data}');
+    print('Message notification: ${message.notification?.title}');
+    print('Message notification: ${message.notification?.body}');
+  }
+}
+
+class FirebaseNotification {
+  // TODO: Request permission
+  // TODO: Register with FCM
+  // TODO: Set up foreground message handler
+  // TODO: Set up background message handler
+
+  static bool _initialized = false;
+  static String? _token;
+
+  static String? get token => _token;
+
+  void initFirebaseMessaging() async {
+    final messaging = FirebaseMessaging.instance;
+
+    final settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    if (kDebugMode) {
+      print('Permission granted: ${settings.authorizationStatus}');
+    }
+    String? token = await messaging.getToken();
+    if (kDebugMode) {
+      print('Registration Token=$token');
+    }
+
+    final _messageStreamController = BehaviorSubject<RemoteMessage>();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      if (kDebugMode) {
+        print('Handling a foreground message: ${message.messageId}');
+        print('Message data: ${message.data}');
+        print('Message notification: ${message.notification?.title}');
+        print('Message notification: ${message.notification?.body}');
+      }
+
+      _messageStreamController.sink.add(message);
+    });
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  }
+}
