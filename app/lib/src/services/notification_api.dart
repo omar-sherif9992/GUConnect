fe5e98c033e2ae7f1f9d1099a79ea7e63a6d73e4
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2023-12-12 06:50:27 +0200

    feat: notification interface

--- app/lib/src/services/notification_api.dart
+++ app/lib/src/services/notification_api.dart
@@ -1,7 +1,11 @@
+import 'dart:ffi';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
@@ -15,17 +19,50 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 }
 
 class FirebaseNotification {
-  // TODO: Request permission
-  // TODO: Register with FCM
-  // TODO: Set up foreground message handler
-  // TODO: Set up background message handler
-
-  static bool _initialized = false;
   static String? _token;
 
   static String? get token => _token;
 
-  void initFirebaseMessaging() async {
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description: 'This channel is used for important notifications.',
+    importance: Importance.max,
+  );
+
+  final _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  Future initializeLocalNotification() async {
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(_androidChannel);
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('logoicon');
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: (payload) async {
+      if (payload != null) {
+        if (kDebugMode) {
+          print('notification payload: $payload');
+        }
+      }
+    });
+  }
+
+  void initNotification() async {
+    await initFirebaseMessaging();
+    await initializeLocalNotification();
+  }
+
+  Future<void> initFirebaseMessaging() async {
     final messaging = FirebaseMessaging.instance;
 
     final settings = await messaging.requestPermission(
@@ -41,11 +78,13 @@ class FirebaseNotification {
       print('Permission granted: ${settings.authorizationStatus}');
     }
     String? token = await messaging.getToken();
+    _token = token;
     if (kDebugMode) {
       print('Registration Token=$token');
     }
 
     final _messageStreamController = BehaviorSubject<RemoteMessage>();
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       if (kDebugMode) {
@@ -54,10 +93,98 @@ class FirebaseNotification {
         print('Message notification: ${message.notification?.title}');
         print('Message notification: ${message.notification?.body}');
       }
-
-      _messageStreamController.sink.add(message);
+      if (message.notification != null) {
+        _flutterLocalNotificationsPlugin.show(
+          message.hashCode,
+          message.notification!.title,
+          message.notification!.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              _androidChannel.id,
+              _androidChannel.name,
+              channelDescription: _androidChannel.description,
+              icon: '@drawable/logoicon',
+            ),
+          ),
+          payload: message.data.toString(),
+        );
+        _messageStreamController.sink.add(message);
+      }
     });
+  }
 
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  static Future<void> testHealth() async {
+    final HttpsCallable callable =
+        FirebaseFunctions.instance.httpsCallable('checkHealth');
+    final results = await callable();
+    if (kDebugMode) print(results.data);
+  }
+
+  static Future<void> sendNotification(
+      String? token, String title, String body) async {
+    final HttpsCallable callable =
+        FirebaseFunctions.instance.httpsCallable('sendNotification');
+    final results = await callable(<String, dynamic>{
+      'token': token,
+      'title': title,
+      'body': body,
+    });
+    if (kDebugMode) print(results.data);
+  }
+
+  static Future<Bool> sendTagNotification(String taggedUserName,
+      String taggedUserToken, String confessionId, String taggerName) async {
+    final HttpsCallable callable =
+        FirebaseFunctions.instance.httpsCallable('sendTagNotification');
+    final results = await callable(<String, dynamic>{
+      'taggedUserName': taggedUserName,
+      'taggedUserToken': taggedUserToken,
+      'confessionId': confessionId,
+      'taggerName': taggerName,
+    });
+    if (kDebugMode) print(results.data);
+    return results.data.success;
+  }
+
+  static Future<Bool> sendLikeNotification(
+      String postOwnerName,
+      String postOwnerToken,
+      String postId,
+      String postType,
+      String likerName) async {
+    final HttpsCallable callable =
+        FirebaseFunctions.instance.httpsCallable('sendLikeNotification');
+    // post type could be [confession, event post, announcement, ...]
+    final results = await callable(<String, dynamic>{
+      'postOwner': {
+        'name': postOwnerName,
+        'token': postOwnerToken,
+      },
+      'postId': postId,
+      'type': postType,
+      'likerName': likerName,
+    });
+    if (kDebugMode) print(results.data);
+    return results.data.success;
+  }
+
+  static Future<Bool> sendPostApprovalNotification(
+      String postOwnerName,
+      String postOwnerToken,
+      String postId,
+      String postType,
+      String approverName) async {
+    final HttpsCallable callable = FirebaseFunctions.instance
+        .httpsCallable('sendPostApprovalNotification');
+    // post type could be [confession, event post, announcement, ...]
+    final results = await callable(<String, dynamic>{
+      'postOwner': {
+        'name': postOwnerName,
+        'token': postOwnerToken,
+      },
+      'postId': postId,
+    });
+    if (kDebugMode) print(results.data);
+    return results.data.success;
   }
 }
